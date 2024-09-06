e71cd029ad5840a0c30f9f53e019686b3a6d089a
Tristan Monteiro <monteirotristan@gmail.com>
2024-09-06 16:53:50 +0200

    Repartir les icones et images dans des dossiers

---
+++ src/assets/images/images-project/images.ts
@@ -0,0 +1,11 @@
+import vera from './project-vera.jpeg'
+import quelleenergie from './project-quelleenergie.png'
+import annuaire from './project-annuaire.png'
+
+const images: Record<string, string> = {
+  vera,
+  quelleenergie,
+  annuaire
+}
+
+export default images
