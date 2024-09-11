b18db506c4e3a67cb4d62f5db6db7ef477593550
Tristan Monteiro <monteirotristan@gmail.com>
2024-09-11 14:52:57 +0200

    Ajoute une partie Formations en timeline vertical

---
+++ src/components/timeline-formation/TimelineFormation.ts
@@ -0,0 +1,4 @@
+import TimelineFormation from './TimelineFormation.vue'
+import './TimelineFormation.scss'
+
+export default TimelineFormation
---
+++ src/components/timeline-formation/TimelineFormation.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+import iconEbi from '@/assets/images/icons/formation/icon-ebi.webp'
+import iconLewagon from '@/assets/images/icons/formation/icon-lewagon.png'
+
+const { t } = useI18n()
+
+const props = defineProps<{
+  formations: string[] | null
+}>()
+
+const getRightImages = (formation: string): string => {
+  if (formation === 'lewagon') {
+    return iconLewagon
+  } else if (formation.includes('ebi')) {
+    return iconEbi
+  }
+  return ''
+}
+</script>
+
+<template>
+  <div class="timeline-container">
+    <h2><span class="numbered-title">01. </span>Mes formations</h2>
+
+    <div class="timeline-details">
+      <ul>
+        <li v-for="(formation, index) in props.formations" :key="index">
+          <div class="timeline-detail">
+            <img alt="Vue logo" class="timeline-logo" :src="getRightImages(formation)" />
+            <div class="timeline-description">
+              <p class="timeline-description-title">
+                {{ t(`formations.${formation}.title`) }}
+              </p>
+              <span class="primary-color-font">{{ t(`formations.${formation}.date`) }}</span>
+              <p>
+                {{ t(`formations.${formation}.details`) }}
+              </p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
