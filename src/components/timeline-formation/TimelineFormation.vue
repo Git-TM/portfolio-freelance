f97fd4d2234755fdeb13d569799ac538c2550cbc
Tristan Monteiro <monteirotristan@gmail.com>
2024-09-12 10:46:56 +0200

    Timeline vertical centré sur gauche si sur mobile, et reglages css formations

--- src/components/timeline-formation/TimelineFormation.vue
+++ src/components/timeline-formation/TimelineFormation.vue
@@ -22,14 +22,13 @@ const getRightImages = (formation: string): string => {
 <template>
   <div class="timeline-container">
     <h2><span class="numbered-title">01. </span>Mes formations</h2>
-
     <div class="timeline-details">
       <ul>
         <li v-for="(formation, index) in props.formations" :key="index">
           <div class="timeline-detail">
             <img alt="Vue logo" class="timeline-logo" :src="getRightImages(formation)" />
             <div class="timeline-description">
-              <p class="timeline-description-title">
+              <p class="timeline-description-title primary-color-font">
                 {{ t(`formations.${formation}.title`) }}
               </p>
               <span class="primary-color-font">{{ t(`formations.${formation}.date`) }}</span>
