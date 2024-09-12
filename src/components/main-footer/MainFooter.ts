dc6a522369dd92336104d203b3bfc76c6a81c286
Tristan Monteiro <monteirotristan@gmail.com>
2024-09-12 11:05:49 +0200

    Ajoute un footer tres leger

---
+++ src/components/main-footer/MainFooter.ts
@@ -0,0 +1,4 @@
+import MainFooter from './MainFooter.vue'
+import './MainFooter.scss'
+
+export default MainFooter
---
+++ src/components/main-footer/MainFooter.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <div class="footer-container">
+    <p>{{ t('footer.first_text') }}</p>
+    <p>{{ t('footer.second_text') }}</p>
+  </div>
+</template>
